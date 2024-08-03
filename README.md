94eaf9655ac86d1cf927560e7752847305d0a622
ALAYODE JIDE <trainneralex7@gmail.com>
2024-08-03 13:03:07 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# JPMC Task 1
+Starter repo for task 1 of the JPMC software engineering program
+=======
+# New-Project
+>>>>>>> 32057dda87ff283719ce5d845dec71404d3d4bac
---
+++ client_test.py
@@ -0,0 +1,33 @@
+import unittest
+from client3 import getDataPoint, getRatio
+
+class ClientTest(unittest.TestCase):
+    def test_getDataPoint_calculatePrice(self):
+        quotes = [
+            {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            self.assertEqual(price, (bid_price + ask_price) / 2)
+
+    def test_getRatio(self):
+        price_a = 120.48
+        price_b = 121.68
+        self.assertEqual(getRatio(price_a, price_b), price_a / price_b)
+        self.assertIsNone(getRatio(price_a, 0))
+
+    def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+        quotes = [
+            {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            self.assertEqual(price, (bid_price + ask_price) / 2)
+
+    """ Additional tests can be added here """
+
+
+if __name__ == '__main__':
+    unittest.main()
